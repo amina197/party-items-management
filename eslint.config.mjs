a94bcceab91b33c1d446461eb96f31220a50e1b9
Amina Thiam <amina.thiam.pro@gmail.com>
2025-01-25 20:44:05 +0100

    configure eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,23 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    files: ['**/*.{js,mjs,cjs,ts}'],
+    rules: {
+      quotes: ['error', 'single'],
+      eqeqeq: ['error', 'always'],
+      'dot-notation': ['error', { 'allowKeywords': false }],
+      'no-console': ['error', { allow: ['error'] }],
+      'no-else-return': ['error'],
+      'no-empty-function': ['error']
+    },
+  },
+  {
+    languageOptions: { globals: globals.browser }
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+];
